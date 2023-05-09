e420522a3c4450f319e1bfdd16eff50ef35d3d2f
MJephat <mukundijephat22@gmail.com>
2023-05-09 11:38:07 +0300

    did the navbar and styled

--- jephatportfolio/src/sections/navbar/NavBar.js
+++ jephatportfolio/src/sections/navbar/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react'
+// import Myphoto from "../../../public/pic/Myphoto.png"
 import "./navbar.css"
+import data from "./data"
+import {IoIosColorPalette} from "react-icons/io"
 
 
 function NavBar() {
   return (
-    <nav><h2>NavBar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className='nav__logo'>
+          <img src={"../../../public/pic/${Myphoto}.png"} alt="logo"/>
+        </a>
+        <ul className='nav__menu'>
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+          </ul>
+        <button id='theme__icon'><IoIosColorPalette/></button>
+          </div> 
+         </nav>
   )
 }
 
