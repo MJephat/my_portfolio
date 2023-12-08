55dcaf8b803ca1e83b023811334158cfcff59c05
MJephat <mukundijephat22@gmail.com>
2023-12-08 13:01:05 +0300

    added a project in portfolio section

--- jephatportfolio/src/sections/portfolio/data.js
+++ jephatportfolio/src/sections/portfolio/data.js
@@ -4,12 +4,12 @@
 import IMG3 from "../../pic/IMG3.png"
 // import IMG4 from "../../pic/IMG4.avif"
 import IMG4 from "../../pic/IMG4.png"
-import IMG7 from "../../pic/IMG7.png"
+import IMG8 from "../../pic/IMG8.png"
 
  
 const data = [
     // {id: 1, image:IMG1, title:"real estate webapp", github:"https://github.com/MJephat",demo:"https://mjephat.github.io/real-estate-project/"},
-    {id: 2, image:IMG7, title:"Travel website", github:"https://github.com/MJephat/landing",demo:"https://landing-d7ym.vercel.app/"},
+    {id: 2, image:IMG8, title:"Travel website", github:"https://github.com/MJephat/landing",demo:"https://landing-d7ym.vercel.app/"},
     // {id: 3, image:IMG3, title:"E-commerce webapp", github:"https://github.com/MJephat",demo:"https://techmart-project.vercel.app/"},
     {id: 3, image:IMG3, title:"Ireporter", github:"https://github.com/bonnkekimani/IReporter",demo:"https://ireporter-full-app.onrender.com"},
     {id: 4, image:IMG4, title:"coffee-shop", github:"https://github.com/MJephat/coffee-shop",demo:"https://coffee-shop-tan.vercel.app/"},
